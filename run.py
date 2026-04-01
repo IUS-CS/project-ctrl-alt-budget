83dc984ee968de195ba7c979f3943dd672fb6539
Thomas Hyde <thhyde@iu.edu>
2026-03-31 23:26:36 -0500

    fixed merge mistake

--- run.py
+++ run.py
@@ -1,31 +1,10 @@
-from flask import Flask
-from dotenv import load_dotenv
-from flask_sqlalchemy import SQLAlchemy
-from src.backend.db.models import db, User
-import os
+# Flask app entry point 
+# In the future we should be able to do python run.py or make run to bootup the app
+from src.backend.app import create_app, db
 
-load_dotenv()
+# Creates the Flask app using the create_app() function in app/app__init__.py
+app = create_app()
 
-# Initializes and runs Flask app server
-
-app = Flask(__name__)
-
-# Database Engine Configuration
-# in .env change DATABASE_URI=mysql+pymysql://user:password@localhost/budget_db to your dev credentials
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
-
-@app.route("/")
-def home():
-
-    # db test
-    all_users = User.query.all()
-    print(all_users)
-    # Setup for route to homepage
-    return "Budget App Backend Running"
-
+# We will import necessary local packages for run.py in future sprints.
 if __name__ == "__main__":
-    # Starts dev server in debug mode
-    app.run(debug = True)
+    app.run(debug=True)
