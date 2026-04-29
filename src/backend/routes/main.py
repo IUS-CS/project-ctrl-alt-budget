37de03eeaea355cb33abf9c1cc1589dd496e6e7b
Steven Maddox - Batz <smaddoxb@iu.edu>
2026-04-29 01:19:28 -0400

    Unify dashboard styling with site theme

--- src/backend/routes/main.py
+++ src/backend/routes/main.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-
+from flask_login import login_required
 # Blueprint homepage and other interactive non-auth or login related pages
 main_bp = Blueprint('main', __name__)
 
@@ -7,3 +7,13 @@ main_bp = Blueprint('main', __name__)
 @main_bp.route("/")
 def home():
     return render_template("homepage.html")
+
+@main_bp.route("/goals")
+@login_required
+def goals():
+    return render_template("goals.html")
+
+@main_bp.route("/reports")
+@login_required
+def reports():
+    return render_template("reports.html")
